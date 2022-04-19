19f17ff58ae9684bc3a2b96f73340c334eebf40c
Pitchakorn Pichetnaruemit <pitchakorn444@gmail.com>
2022-04-19 17:58:32 +0700

    Import page for react route

--- company-registration/src/components/sidebar/index.js
+++ company-registration/src/components/sidebar/index.js
@@ -1,5 +1,16 @@
 import { ProSidebar, Menu, MenuItem, SubMenu, SidebarHeader, SidebarContent } from "react-pro-sidebar";
-import 'react-pro-sidebar/dist/css/styles.css';
+import { Routes, Route, Link } from "react-router-dom";
+
+import Login from "../loginScreen"
+import CreateCompany from "../create-company.component"
+import EditCompany from "../edit-company.component"
+import CompanyList from "../company-list.component"
+import AddStaff from "../add-account.component"
+import MapPage from "../map/map.component"
+import AddDistCenter from "../add-dist-center.component"
+import ViewStaff from "../view-staff.component"
+import { OverviewPage } from "../../pages/Overview";
+import { ShipmentPage } from "../../pages/Shipment";
 
 
 export const CustomSidebar = () => {
