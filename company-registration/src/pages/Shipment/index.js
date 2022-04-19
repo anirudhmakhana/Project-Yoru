b891f4af9fda8160e484ce46af8073c66801a37e
Pitchakorn Pichetnaruemit <pitchakorn444@gmail.com>
2022-04-19 17:57:31 +0700

    Add shipment page

---
+++ company-registration/src/pages/Shipment/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import axios from 'axios';
+
+// import "../../assets/style/overview.css"
+
+export const ShipmentPage = () => {
+    return (
+        <div id="shipment">
+            <h1>Shipment</h1>
+        </div>
+    );
+}
