bb69eb57b84dd7bcb1d48ff083d4446df97ec04e
Pitchakorn Pichetnaruemit <pitchakorn444@gmail.com>
2022-04-19 17:54:41 +0700

    Replace navbar with sticky sidebar

--- company-registration/src/App.js
+++ company-registration/src/App.js
@@ -20,71 +20,20 @@ import AddDistCenter from "./components/add-dist-center.component"
 import ViewStaff from "./components/view-staff.component"
 import { OverviewPage } from './pages/Overview';
 
+import { CustomSidebar } from './components/sidebar';
+
 const createError = require('http-errors')
 
 function App() {
   return (
+    
     <BrowserRouter>
       <div className="App">
-
-          <NavBar bg = "dark" variant="dark">
-            <Container>
-
-              <NavBar.Brand>
-                <Link to={"/"} className="nav-link">
-                  Project Yoru
-                </Link>
-              </NavBar.Brand>
-
-              <Nav className="justify-content-end">
-                <Nav>
-                  <Link to={"/map"} class="nav-link">
-                    Map
-                  </Link>
-                </Nav>
-                <Nav>
-                  <Link to={"/create-company"} class="nav-link">
-                    Create company
-                  </Link>
-                </Nav>
-                <Nav>
-                  <Link to={"/company-list"} class="nav-link">
-                    Company List
-                  </Link>
-                </Nav>
-                <Nav>
-                  <Link to={"/overview"} class="nav-link">
-                    Overview
-                  </Link>
-                </Nav>
-              </Nav>
-
-            </Container>
-          </NavBar>
-          
-        <Container>
-          <Row>
-            <Col md="12">
-              <div className="wrapper">
-                <Routes>
-                  <Route exact path="/" element={<Login/>}/>
-                  <Route path="/create-company" element={<CreateCompany/>}/>
-                  <Route path="/edit-company/:id" element={<EditCompany/>}/>
-                  <Route path="/company-list" element={<CompanyList/>}/>
-                  <Route path="/add-staff/:id" element={<AddStaff/>}/>
-                  <Route path="/view-staff/:id" element={<ViewStaff/>}/>
-                  <Route path="/add-dist-center/:id" element={<AddDistCenter/>}/>
-                  <Route path="/map/" element={<MapPage/>}/>
-                  <Route path="/overview" element={<OverviewPage/>}/>
-
-                </Routes>
-              </div>
-            </Col>
-          </Row>
-        </Container>
-
+        <CustomSidebar/>
       </div>
     </BrowserRouter>
+    
+    
   );
 }
 
