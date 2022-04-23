423454f89c0c6e89c6f087bca6e0e02ae7d0099d
Pitchakorn Pichetnaruemit <pitchakorn444@gmail.com>
2022-04-24 01:52:35 +0700

    Add card and Line graph

--- company-registration/src/pages/Overview/index.js
+++ company-registration/src/pages/Overview/index.js
@@ -4,10 +4,24 @@ import axios from 'axios';
 
 import "../../assets/style/overview.css"
 
+import { Card } from "../../components/card";
+import { FrequencyChart } from "../../components/chart";
+
 export const OverviewPage = () => {
     return (
         <div id="overview">
-            <h1>Overview</h1>
+            <div className="title-container">
+                <h1>Overview</h1>
+            </div>
+            <div className="body-top">
+                <Card title="In transit" info="0"/>
+                <Card title="Shipped" info="0"/>
+                <Card title="Delayed" info="0"/>
+                <Card title="On hold" info="0"/>
+            </div>
+            <div className="body-main">
+                <FrequencyChart/>
+            </div>
         </div>
     );
 }
