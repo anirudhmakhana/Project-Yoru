4d7cec4e2992a0b952be31ccf59f5a593c5b8d19
Pitchakorn Pichetnaruemit <pitchakorn444@gmail.com>
2022-04-24 02:50:56 +0700

    Update login html

--- company-registration/src/pages/Login/index.js
+++ company-registration/src/pages/Login/index.js
@@ -7,20 +7,27 @@ export const LoginPage = () => {
     return (
         <div className="loginBackground">
             <div className="startPageContainer">
-                <h1>Project Yoru</h1>
+                <div className="logo">
+                    <h3>Project Yoru</h3>
+                </div>
 
                 <h2>Log In to Project Yoru</h2>
                 <p>Enter your email and password below</p>
                 <form>
-                    <div className="textInputContainer">
-                        <label for="email">Email</label>
-                        <input type="text" id="email" name="email" placeholder="Email"></input>
+                    <div className="textInputContainerCol">
+                        <label className="inputLabel" for="email">Email</label>
+                        <input type="text" id="email" name="email" placeholder="Email Address"></input>
                     </div>
-                    <div className="textInputContainer"> 
-                        <label for="password">Password</label>
+                    <div className="textInputContainerCol"> 
+                        <label className="inputLabel" for="password">Password</label>
                         <input type="text" id="password" name="password" placeholder="Password"></input>
                     </div>
-                    <input type="submit" value="Log In"></input>
+                    <input className="signinBtn" type="submit" value="Log In"></input>
+                    <div className="buttonContainerRow">
+                        <label>Don't have an account ?</label>
+                        <input className="signupBtn" type="submit" value="Sign Up"></input> 
+                    </div>
+                    
                 </form>
             </div>
         </div>
