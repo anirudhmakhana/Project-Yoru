409267fa918e826061150bbdebc83113f53cdfb4
Pitchakorn Pichetnaruemit <pitchakorn444@gmail.com>
2022-04-25 17:02:30 +0700

    Create main page

---
+++ company-registration/src/pages/Main/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import { CustomSidebar } from "../../components/sidebar";
+
+export const MainPage = () => {
+    return (
+        <>
+            <CustomSidebar/>
+        </>
+    );
+}
