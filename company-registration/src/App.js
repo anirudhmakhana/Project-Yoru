1f2cf1d91eddef180278fca75033868c5bffcf93
Pitchakorn Pichetnaruemit <pitchakorn444@gmail.com>
2022-04-26 14:47:38 +0700

    Update routes

--- company-registration/src/App.js
+++ company-registration/src/App.js
@@ -5,9 +5,13 @@ import 'bootstrap/dist/css/bootstrap.css'
 
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
+
 import { LoginPage } from './pages/Login';
 import { RegisterPage } from './pages/Register';
 import { MainPage } from './pages/Main';
+
+import { OverviewPage } from "./pages/Overview";
+import { ShipmentPage } from "./pages/Shipment";
 
 const createError = require('http-errors')
 
@@ -18,8 +22,12 @@ function App() {
       <div className="App">
         <Routes>
           <Route exact path="/" element={<LoginPage/>} />
-          <Route path="/register" element={<RegisterPage/>} />
-          <Route path="/main" element={<MainPage/>} />
+          <Route path="register" element={<RegisterPage/>} />
+          <Route path="main" element={<MainPage/>}>
+            <Route index element={<OverviewPage/>}/>
+            <Route path="overview" element={<OverviewPage/>}/>
+            <Route path="shipment" element={<ShipmentPage/>}/>
+          </Route>
         </Routes>
       </div>
     </BrowserRouter>
