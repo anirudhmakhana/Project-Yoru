3f9e4e17b0c81f421292014eeef135709048d592
Pitchakorn Pichetnaruemit <pitchakorn444@gmail.com>
2022-04-27 14:58:06 +0700

    Fix conflict

--- company-registration/src/App.js
+++ company-registration/src/App.js
@@ -9,11 +9,9 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { LoginPage } from './pages/Login';
 import { RegisterPage } from './pages/Register';
 import { MainPage } from './pages/Main';
-
-import { OverviewPage } from "./pages/Overview";
-import { ShipmentPage } from "./pages/Shipment";
 import { AdminLoginPage } from './pages/AdminLogin';
-import CompanyList  from "./components/company-list.component"
+
+
 const createError = require('http-errors')
 
 function App() {
@@ -25,17 +23,8 @@ function App() {
           <Route exact path="/" element={<LoginPage/>} />
           <Route exact path="/admin" element={<AdminLoginPage/>} />
           <Route path="register" element={<RegisterPage/>} />
-          <Route path="main" element={<MainPage/>}>
-            <Route index element={<OverviewPage/>}/>
-            <Route path="overview" element={<OverviewPage/>}/>
-            <Route path="shipment" element={<ShipmentPage/>}/>
-          </Route>
-
-          <Route path="admin/main" element={<MainPage/>}>
-            <Route index element={<CompanyList/>}/>
-            <Route path="company-list" element={<CompanyList/>}/>
-            {/* <Route path="shipment" element={<ShipmentPage/>}/> */}
-          </Route>
+          <Route path="main/*" element={<MainPage/>}/>
+          <Route path="admin/main/*" element={<MainPage/>}/>          
         </Routes>
       </div>
     </BrowserRouter>
