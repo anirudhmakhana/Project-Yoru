5c291a536af82374076876f3fa1b38faf3ac33f2
Pitchakorn Pichetnaruemit <pitchakorn444@gmail.com>
2022-04-30 03:34:56 +0700

    Add classname to html component

--- company-registration/src/components/card/index.js
+++ company-registration/src/components/card/index.js
@@ -4,8 +4,8 @@ export const Card = (props) => {
     return(
         <div className="card">
             <div className="card-body">
-                <h2>{props.title}</h2>
-                <p>{props.info}</p>
+                <h2 className="card-title">{props.title}</h2>
+                <p className="card-info">{props.info}</p>
             </div>
         </div>
     );
