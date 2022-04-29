f84be42018584cb6d34e5aafd26c528d5c41e7dc
Pitchakorn Pichetnaruemit <pitchakorn444@gmail.com>
2022-04-30 00:24:16 +0700

    Update route

--- company-registration/src/components/sidebar/index.js
+++ company-registration/src/components/sidebar/index.js
@@ -1,8 +1,14 @@
 import { ProSidebar, Menu, MenuItem, SubMenu, SidebarHeader, SidebarContent } from "react-pro-sidebar";
 import { Routes, Route, Link } from "react-router-dom";
 
-import { OverviewPage } from "../../pages/Overview";
+import { OverviewPage } from "../../pages/Overview"
 import { ShipmentListPage } from "../../pages/ShipmentList";
+import { CreateSHP } from "../../pages/CreateShipment";
+import { UpdateSHP } from "../../pages/UpdateShipment";
+import { CancelSHP } from "../../pages/CancelShipment";
+import { LocationPage } from "../../pages/Location";
+
+
 import { RegisterPage} from "../../pages/Register"
 import { AddStaffPage } from "../../pages/AddStaff";
 import { StaffListPage } from "../../pages/StaffList";
@@ -32,10 +38,10 @@ export const CustomSidebar = (props) => {
         const initialIndex = 
             window.location.pathname === "overview" ? 0 
             : window.location.pathname === "shipment" ? 1 
-            : window.location.pathname === '/' ? 2 
-            : window.location.pathname === '/' ? 3 
-            : window.location.pathname === '/' ? 4
-            : window.location.pathname === '/' ? 5 
+            : window.location.pathname === 'create' ? 2 
+            : window.location.pathname === 'update' ? 3 
+            : window.location.pathname === 'cancel' ? 4
+            : window.location.pathname === 'location' ? 5 
             : window.location.pathname === 'edit-staff' ? 6 
             : window.location.pathname === 'staff-list' ? 7 
             : window.location.pathname === '/' ? 8 
@@ -61,16 +67,16 @@ export const CustomSidebar = (props) => {
                         <Link to={"shipment"} onClick={() => {setActiveItemIndex(1)}}>Shipment</Link>
                     </MenuItem>
                     <MenuItem active={activeItemIndex === 2} icon={<FontAwesomeIcon icon={faCirclePlus}/>}>
-                        <Link to={"/"} onClick={() => {setActiveItemIndex(2)}}>Create Shipment</Link>
+                        <Link to={"create"} onClick={() => {setActiveItemIndex(2)}}>Create Shipment</Link>
                     </MenuItem>
                     <MenuItem active={activeItemIndex === 3} icon={<FontAwesomeIcon icon={faPen}/>}>
-                        <Link to={"/"} onClick={() => {setActiveItemIndex(3)}}>Update Shipment</Link>
+                        <Link to={"update"} onClick={() => {setActiveItemIndex(3)}}>Update Shipment</Link>
                     </MenuItem>
                     <MenuItem active={activeItemIndex === 4} icon={<FontAwesomeIcon icon={faBan}/>}>
-                        <Link to={"/"} onClick={() => {setActiveItemIndex(4)}}>Cancel Shipment</Link>
+                        <Link to={"cancel"} onClick={() => {setActiveItemIndex(4)}}>Cancel Shipment</Link>
                     </MenuItem>
                     <MenuItem active={activeItemIndex === 5} icon={<FontAwesomeIcon icon={faLocationDot}/>}>
-                        <Link to={"/"} onClick={() => {setActiveItemIndex(5)}}>Place & Location</Link>
+                        <Link to={"location"} onClick={() => {setActiveItemIndex(5)}}>Place & Location</Link>
                     </MenuItem>
                     <MenuItem active={activeItemIndex === 6} icon={<FontAwesomeIcon icon={faAddressCard}/>}>
                         <Link to={"edit-staff/"+userData.companyCode+"/"+userData.username} onClick={() => {setActiveItemIndex(6)}}>Edit Account</Link>
@@ -92,6 +98,10 @@ export const CustomSidebar = (props) => {
             <Routes>
                 <Route path="overview" element={<OverviewPage/>}/>
                 <Route path="shipment" element={<ShipmentListPage/>}/>
+                <Route path="create" element={<CreateSHP/>}/>
+                <Route path="update" element={<UpdateSHP/>}/>
+                <Route path="cancel" element={<CancelSHP/>}/>
+                <Route path="location" element={<LocationPage/>}/>
                 <Route path="edit-staff/:companyCode/:username" element={<EditStaffPage/>}/>
                 {/* <Route path="register-staff/:companyCode" element={<AddStaffPage />}/> 
                 <Route path="staff-list/:companyCode" element={<StaffListPage />}/> 
