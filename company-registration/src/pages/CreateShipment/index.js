3f2b42e7a0cca4b86bedff3b593c5d30d78ccef2
anirudhkmitl <makhana.anirudh@gmail.com>
2022-05-06 13:42:33 +0700

    [ACCEPTED]: Changes

--- company-registration/src/pages/CreateShipment/index.js
+++ company-registration/src/pages/CreateShipment/index.js
@@ -1,9 +1,9 @@
 import React from "react";
 
 export const CreateSHP = () => {
-    return (
-        <>
-            <h1>Create Shipment</h1>
-        </>
-    );
-}
+	return (
+		<>
+			<h1>Create Shipment</h1>
+		</>
+	);
+};
