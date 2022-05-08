de5b6d52f3e7ca07b5e4243177b4bf61f491238b
Pitchakorn Pichetnaruemit <pitchakorn444@gmail.com>
2022-05-08 18:27:17 +0700

    Add div wrapper

--- company-registration/src/pages/ScanShipment/index.js
+++ company-registration/src/pages/ScanShipment/index.js
@@ -2,8 +2,13 @@ import React from "react";
 
 export const ScanSHP = () => {
     return (
-        <>
-            <h1>Scan to link to update shipment page</h1>
-        </>
+        <div className="content-main-container">
+            <div className="content-title-container">
+                <h1>Scan to link to update shipment page</h1>
+            </div>
+           <div className="detailed-main-container">
+                
+           </div>
+        </div>
     );
 }
