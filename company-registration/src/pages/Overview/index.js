a17a54b7046a6772b887640f88d47109d4c6fedb
Anirudh <makhana.anirudh@gmail.com>
2022-05-22 14:41:29 +0700

    [FIXED]: Merge Conflicts

--- company-registration/src/pages/Overview/index.js
+++ company-registration/src/pages/Overview/index.js
@@ -19,40 +19,30 @@ import { EditProfilePopup } from "../../components/edit_profile_popup";
 // In this case, January = 0
 
 export const OverviewPage = (props) => {
-    const [userData, setUserData] = useState(eval('('+localStorage.getItem("userData")+')'))
-    const [currentNodeCode, setCurrentNodeCode] = useState(null)
-    const [nodePopup, setNodePopup] = useState(false);
-    const [editProfPopup, setEditProfPopup] = useState(false);
+	const [userData, setUserData] = useState(
+		eval("(" + localStorage.getItem("userData") + ")")
+	);
+	const [currentNodeCode, setCurrentNodeCode] = useState(null);
+	const [nodePopup, setNodePopup] = useState(false);
+	const [editProfPopup, setEditProfPopup] = useState(false);
 
-    const [dateGraphData, setDateGraphData] = useState(null)
-    // const [hourGraphData, setHourGraphData] = useState(null)
-    const [currentDate, setCurrentDate] = useState( new Date() )
-    const [graphTimeRange, setGraphTimeRange] = useState("day")
-    const [graphType, setGraphType] = useState("shipping")
-    
-    const [completedCount, setCompletedCount] = useState(0)
-    const [stockCount, setStockCount] = useState(0)
-    const [incompleteCount, setIncompleteCount] = useState(0)
-    const [shippingCount, setShippingCount] = useState(0)
-    const [primGraphStartDate, setPrimStartDate] = useState('')
-    const [primGraphEndDate, setPrimEndDate] = useState('')
-    const [primHighest, setPrimHighest] = useState(0)
-    const [primAverage, setPrimAverage] = useState(0)
-    const [primLowest, setPrimLowest] = useState(0)
-    const [primChangePercent, setPrimChangePercent] = useState(0)
-    const [primTotal, setTotal] = useState(0)
-    // useState(() => {
-    //     var node = eval('('+localStorage.getItem("currentNode")+')')
-    //     if (node) {
-    //         setCurrentNodeCode(node.nodeCode)
-    //     } else {
-    //         setCurrentNodeCode('-')
-    //     }
-    // }, [])
-    const handleGraphType = (e) => {
-        setGraphType(e)        
-    };
+	const [dateGraphData, setDateGraphData] = useState(null);
+	// const [hourGraphData, setHourGraphData] = useState(null)
+	const [currentDate, setCurrentDate] = useState(new Date());
+	const [graphTimeRange, setGraphTimeRange] = useState("day");
+	const [graphType, setGraphType] = useState("shipping");
 
+	const [completedCount, setCompletedCount] = useState(0);
+	const [stockCount, setStockCount] = useState(0);
+	const [incompleteCount, setIncompleteCount] = useState(0);
+	const [shippingCount, setShippingCount] = useState(0);
+	const [primGraphStartDate, setPrimStartDate] = useState("");
+	const [primGraphEndDate, setPrimEndDate] = useState("");
+	const [primHighest, setPrimHighest] = useState(0);
+	const [primAverage, setPrimAverage] = useState(0);
+	const [primLowest, setPrimLowest] = useState(0);
+	const [primChangePercent, setPrimChangePercent] = useState(0);
+	const [primTotal, setTotal] = useState(0);
 	// useState(() => {
 	//     var node = eval('('+localStorage.getItem("currentNode")+')')
 	//     if (node) {
@@ -65,60 +55,82 @@ export const OverviewPage = (props) => {
 		setGraphType(e);
 	};
 
-    useEffect(() => {
-        if ( eval('('+localStorage.getItem("currentNode")+')')) {
-            setCurrentNodeCode(eval('('+localStorage.getItem("currentNode")+')').nodeCode)
+	const handleTimeRangeDropdown = (e) => {
+		setGraphTimeRange(e);
+	};
 
-        }
-    }, [])
+	useEffect(() => {
+		if (eval("(" + localStorage.getItem("currentNode") + ")")) {
+			setCurrentNodeCode(
+				eval("(" + localStorage.getItem("currentNode") + ")").nodeCode
+			);
+		}
+	}, []);
 
-    
-    useEffect(() => {
-        
-        ShipmentService.completedCountByCompany(userData.companyCode, userData.token)
-        .then( res => {
-            setCompletedCount(res.data)
-        })
-        .catch( err => {
-            console.log(err)
-        })
-        ShipmentService.currentStockCountByCompany(userData.companyCode, userData.token)
-        .then( res => {
-            console.log(res.data)
-            setStockCount(res.data)
-        })
-        .catch( err => {
-            console.log(err)
-        })
-        ShipmentService.shippingCountByCompany(userData.companyCode, userData.token)
-        .then( res => {
-            setShippingCount(res.data)
-        })
-        .catch( err => {
-            console.log(err)
-        })
-        ShipmentService.incompleteCountByCompany(userData.companyCode, userData.token)
-        .then( res => {
-            setIncompleteCount(res.data)
-        })
-        .catch( err => {
-            console.log(err)
-        })
-        GraphService.generateGraph( graphType, graphTimeRange, userData.token, userData.companyCode, null)
-        .then( res => {
-            console.log(res.data)
-            setDateGraphData(res.data.graph)
-            setPrimStartDate(res.data.startDate)
-            setPrimEndDate(res.data.endDate)
-            setPrimAverage(res.data.average)
-            setPrimHighest(res.data.highest)
-            setPrimLowest(res.data.lowest)
-            setPrimChangePercent(res.data.percentageChange)
-            setTotal(res.data.total)
+	useEffect(() => {
+		ShipmentService.completedCountByCompany(
+			userData.companyCode,
+			userData.token
+		)
+			.then((res) => {
+				setCompletedCount(res.data);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+		ShipmentService.currentStockCountByCompany(
+			userData.companyCode,
+			userData.token
+		)
+			.then((res) => {
+				console.log(res.data);
+				setStockCount(res.data);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+		ShipmentService.shippingCountByCompany(userData.companyCode, userData.token)
+			.then((res) => {
+				setShippingCount(res.data);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+		ShipmentService.incompleteCountByCompany(
+			userData.companyCode,
+			userData.token
+		)
+			.then((res) => {
+				setIncompleteCount(res.data);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+		GraphService.generateGraph(
+			graphType,
+			graphTimeRange,
+			userData.token,
+			userData.companyCode,
+			null
+		).then((res) => {
+			console.log(res.data);
+			setDateGraphData(res.data.graph);
+			setPrimStartDate(res.data.startDate);
+			setPrimEndDate(res.data.endDate);
+			setPrimAverage(res.data.average);
+			setPrimHighest(res.data.highest);
+			setPrimLowest(res.data.lowest);
+			setPrimChangePercent(res.data.percentageChange);
+			setTotal(res.data.total);
+		});
+	}, [graphType, graphTimeRange]);
 
-        })
-        
-    }, [graphType,graphTimeRange])
+	function handlePopupConfirm(currentNode) {
+		localStorage.setItem("currentNode", JSON.stringify(currentNode));
+		setCurrentNodeCode(
+			eval("(" + localStorage.getItem("currentNode") + ")").nodeCode
+		);
+		// window.location.reload(false);
 
 		setNodePopup(false);
 	}
@@ -139,40 +151,28 @@ export const OverviewPage = (props) => {
 		setEditProfPopup(false);
 	}
 
-        setNodePopup(false)
-    }
-    
-    function handlePopupCancel() {
-        console.log(localStorage)
-        setNodePopup(false)
-    }
+	function getChartItem(header, stDate, enDate, number = 0, additional = "") {
+		return (
+			<div className="chart-item">
+				<span>
+					<strong>{header}</strong>
+				</span>
+				{stDate == enDate ? (
+					<p>({stDate})</p>
+				) : (
+					<p>
+						({stDate}-{enDate})
+					</p>
+				)}
+				<h2 style={{ "margin-top": "10px" }}>{number}</h2>
+				<p>{additional}</p>
+			</div>
+		);
+	}
 
-    function handleEditProfConfirm(newProfile) {
-        localStorage.setItem("userData", JSON.stringify(newProfile))
-        setUserData(eval('('+localStorage.getItem("userData")+')'))
-        setEditProfPopup(false)
-
-    }
-    
-    function handleEditProfCancel() {
-        console.log(localStorage)
-        setEditProfPopup(false)
-    }
-
-    function getChartItem( header, stDate, enDate, number = 0, additional = '' ){
-        return <div className="chart-item">
-                <span><strong>{header}</strong></span>
-                {stDate == enDate ? 
-                <p>({stDate})</p> :
-                <p>({stDate}-{enDate})</p>}
-                <h2 style={{"margin-top":"10px"}}>{number}</h2>
-                <p>{additional}</p>
-            </div>
-    }
-
-    return (
-        <div className="overview content-main-container">
-            {/* <div className="content-title-container">
+	return (
+		<div className="overview content-main-container">
+			{/* <div className="content-title-container">
                 <h1>Overview</h1>
                 <button onClick={() => setButtonPopup(true)} className="node-select-button">
                     <FontAwesomeIcon icon={faPen} className="node-select-icon"/>Current Node: {currentNodeCode}
@@ -226,37 +226,79 @@ export const OverviewPage = (props) => {
                                     <Dropdown.Item eventKey={"week"}>Week</Dropdown.Item>
                                     <Dropdown.Item eventKey={"month"}>Month</Dropdown.Item>
                                     <Dropdown.Item eventKey={"year"}>Year</Dropdown.Item> */}
-                                </Dropdown.Menu>
-                            </Dropdown>
-                        </div>
-                        {primGraphEndDate == primGraphStartDate ? 
-                        <p>{primGraphStartDate}</p> :
-                        <p>{primGraphStartDate}-{primGraphEndDate}</p>}
-                    </div>
-                    <div className="body-chart-container">
-                        { dateGraphData && <LineChart chartDataPrim={dateGraphData} indicatorX={GraphService.xAxisLabel[graphTimeRange]} indicatorY={GraphService.yAxisLabel[graphType]}/>}
-                    </div>
-                </div>
-                <div className="chart-info-right">
-                    {/* <hr/> */}
-                    
-                    {graphType != 'stock' && getChartItem(`Total Shipment`, primGraphStartDate, primGraphEndDate, primTotal)}
-                    {/* <hr/> */}
-                    {getChartItem(`Highest ${GraphService.graphName[graphType]}`, primGraphStartDate, primGraphEndDate, primHighest.value,)}
-                    {/* <hr/> */}
-                    {getChartItem(`Lowest ${GraphService.graphName[graphType]}`, primGraphStartDate, primGraphEndDate, primLowest.value,)}
-                    {/* <hr/> */}
-                    {getChartItem(`Average ${GraphService.graphName[graphType]}`, primGraphStartDate, primGraphEndDate, primAverage.toPrecision(2))}
-                    {/* <hr/> */}
-                    {/* {getChartItem(`Percentage Changes`, primGraphStartDate, primGraphEndDate, `${primChangePercent}%`)} */}
-                    {/* <hr/> */}
+								</Dropdown.Menu>
+							</Dropdown>
+						</div>
+						{primGraphEndDate == primGraphStartDate ? (
+							<p>{primGraphStartDate}</p>
+						) : (
+							<p>
+								{primGraphStartDate}-{primGraphEndDate}
+							</p>
+						)}
+					</div>
+					<div className="body-chart-container">
+						{dateGraphData && (
+							<LineChart
+								chartDataPrim={dateGraphData}
+								indicatorX={GraphService.xAxisLabel[graphTimeRange]}
+								indicatorY={GraphService.yAxisLabel[graphType]}
+							/>
+						)}
+					</div>
+				</div>
+				<div className="chart-info-right">
+					{/* <hr/> */}
 
-                    {/* <hr/> */}
-                </div>
-            </div>
-            { nodePopup && <NodeSelectPopup setOpenPopup={setNodePopup} handleConfirm={handlePopupConfirm} handleCancel={handlePopupCancel} />}
-            { editProfPopup && <EditProfilePopup setOpenPopup={setEditProfPopup} handleConfirm={handleEditProfConfirm} handleCancel={handleEditProfCancel} />}
+					{graphType != "stock" &&
+						getChartItem(
+							`Total Shipment`,
+							primGraphStartDate,
+							primGraphEndDate,
+							primTotal
+						)}
+					{/* <hr/> */}
+					{getChartItem(
+						`Highest ${GraphService.graphName[graphType]}`,
+						primGraphStartDate,
+						primGraphEndDate,
+						primHighest.value
+					)}
+					{/* <hr/> */}
+					{getChartItem(
+						`Lowest ${GraphService.graphName[graphType]}`,
+						primGraphStartDate,
+						primGraphEndDate,
+						primLowest.value
+					)}
+					{/* <hr/> */}
+					{getChartItem(
+						`Average ${GraphService.graphName[graphType]}`,
+						primGraphStartDate,
+						primGraphEndDate,
+						primAverage.toPrecision(2)
+					)}
+					{/* <hr/> */}
+					{/* {getChartItem(`Percentage Changes`, primGraphStartDate, primGraphEndDate, `${primChangePercent}%`)} */}
+					{/* <hr/> */}
 
-        </div>
-    );
-}
+					{/* <hr/> */}
+				</div>
+			</div>
+			{nodePopup && (
+				<NodeSelectPopup
+					setOpenPopup={setNodePopup}
+					handleConfirm={handlePopupConfirm}
+					handleCancel={handlePopupCancel}
+				/>
+			)}
+			{editProfPopup && (
+				<EditProfilePopup
+					setOpenPopup={setEditProfPopup}
+					handleConfirm={handleEditProfConfirm}
+					handleCancel={handleEditProfCancel}
+				/>
+			)}
+		</div>
+	);
+};
