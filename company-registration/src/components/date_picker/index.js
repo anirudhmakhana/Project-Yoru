5af221263f5decca8a7e55bbe7e5afc221044d42
Pitchakorn Pichetnaruemit <pitchakorn444@gmail.com>
2022-05-23 18:48:22 +0700

    Change date & time to date only

--- company-registration/src/components/date_picker/index.js
+++ company-registration/src/components/date_picker/index.js
@@ -4,23 +4,24 @@ import TextField from '@mui/material/TextField';
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 
 import Stack from '@mui/material/Stack';
 
-export const DatePicker = () => {
+export const ChartDatePicker = () => {
     const [value, setValue] = useState(
-        new Date('2022-01-01T00:00:00.000Z')
+        new Date('2022-01-01')
     );
 
     return (
         <LocalizationProvider dateAdapter={AdapterDateFns}>
             <Stack spacing={3}>
-            <DateTimePicker
-                label="Date & Time"
+            <DatePicker
+                label="Date"
                 renderInput={(params) => <TextField {...params} />}
                 value={value}
                 onChange={(newValue) => {
-                setValue(newValue);
+                    setValue(newValue);
                 }}
             />
             </Stack>
