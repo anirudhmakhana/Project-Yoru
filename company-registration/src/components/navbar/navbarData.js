d9cb1de518dfbb895b245a1a8f79e050f1bb149a
Pitchakorn Pichetnaruemit <pitchakorn444@gmail.com>
2022-05-23 22:53:04 +0700

    Add data for more user type

--- company-registration/src/components/navbar/navbarData.js
+++ company-registration/src/components/navbar/navbarData.js
@@ -58,3 +58,44 @@ export const navbarManagerData = [
         cName: 'nav-text'
     },
 ]
+
+export const navbarStaffData = [
+    {
+        title: 'Overview',
+        path: '/main/overview',
+        icon: <FontAwesomeIcon icon={faChartPie}/>,
+        cName: 'nav-text'
+    },
+    {
+        title: 'Shipment',
+        path: '/main/shipment',
+        icon: <FontAwesomeIcon icon={faCubes}/>,
+        cName: 'nav-text'
+    },
+    {
+        title: 'Create Shipment',
+        path: '/main/create',
+        icon: <FontAwesomeIcon icon={faCirclePlus}/>,
+        cName: 'nav-text'
+    },
+    {
+        title: 'Update Shipment',
+        path: '/main/scan',
+        icon: <FontAwesomeIcon icon={faPen}/>,
+        cName: 'nav-text'
+    },
+    {
+        title: 'Cancel Shipment',
+        path: '/main/cancel',
+        icon: <FontAwesomeIcon icon={faBan}/>,
+        cName: 'nav-text'
+    },
+    {
+        title: 'Place & Location',
+        path: '/main/node',
+        icon: <FontAwesomeIcon icon={faLocationDot}/>,
+        cName: 'nav-text'
+    },
+]
+
+export const navbarAdminData = []
