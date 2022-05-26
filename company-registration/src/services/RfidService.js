5e542c836b1ad8f80ab5cea5c0439eabef7de48f
Anirudh <makhana.anirudh@gmail.com>
2022-05-26 14:04:59 +0700

    [ACCEPTED]: Changes

--- company-registration/src/services/RfidService.js
+++ company-registration/src/services/RfidService.js
@@ -1,21 +1,19 @@
 import axios from "axios";
 
 class RfidService {
+	constructor() {
+		this.scanUrl =
+			"http://17cd-2001-fb1-ff-d847-f401-1d3a-fd3d-fa2c.ngrok.io/radioData";
+	}
 
-    constructor() {
-        this.scanUrl = 'http://17cd-2001-fb1-ff-d847-f401-1d3a-fd3d-fa2c.ngrok.io/radioData'
-    }
-
-
-    async makeScan() { 
-        // const response = await axios.get(this.scanUrl)
-        // .catch((error) => {
-        //     throw error
-        // })
-        // return response
-        return {data:{statusCode:200, data:{uid:'SHP004'}}}
-    }
-
+	async makeScan() {
+		// const response = await axios.get(this.scanUrl)
+		// .catch((error) => {
+		//     throw error
+		// })
+		// return response
+		return { data: { statusCode: 200, data: { uid: "SHP007" } } };
+	}
 }
 
-export default new RfidService()
+export default new RfidService();
