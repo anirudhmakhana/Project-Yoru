5130fe1f3760c52be72b15aa9be85a6fe86f88ad
Pitchakorn Pichetnaruemit <pitchakorn444@gmail.com>
2022-05-27 08:41:36 +0700

    Update ApiUrl.js

--- company-registration/src/utils/ApiUrl.js
+++ company-registration/src/utils/ApiUrl.js
@@ -1,4 +1,4 @@
 export const serverBasedURL =
-	"https://8509-27-55-77-234.ap.ngrok.io/";
+	"https://8509-27-55-77-234.ap.ngrok.io";
 export const rfidURL =
 	"http://17cd-2001-fb1-ff-d847-f401-1d3a-fd3d-fa2c.ngrok.io/radioData";
